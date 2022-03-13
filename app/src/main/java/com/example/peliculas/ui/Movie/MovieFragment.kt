a7079aeaf8d63efea4af7d80372b10842b7c2495
Juan Mari <juannpmari@gmail.com>
2022-03-13 17:29:58 -0300

    App funcionando

--- app/src/main/java/com/example/peliculas/ui/Movie/MovieFragment.kt
+++ app/src/main/java/com/example/peliculas/ui/Movie/MovieFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ConcatAdapter
 import com.example.peliculas.R
 import com.example.peliculas.databinding.FragmentMovieBinding
@@ -55,12 +56,36 @@ class MovieFragment : Fragment(R.layout.fragment_movie), MovieAdapter.OnMovieCli
                 is Resource.Succes -> {
                     binding.progressBar.visibility = View.GONE
                     concatAdapter.apply {
-                        addAdapter(0, UpcomingConcatAdapter(MovieAdapter(result.data.first.results,this@MovieFragment)))
-                        addAdapter(0, TopRatedConcatAdapter(MovieAdapter(result.data.second.results,this@MovieFragment)))
-                        addAdapter(0, PopularConcatAdapter(MovieAdapter(result.data.third.results,this@MovieFragment)))
+                        addAdapter(
+                            0,
+                            UpcomingConcatAdapter(
+                                MovieAdapter(
+                                    result.data.first.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            0,
+                            TopRatedConcatAdapter(
+                                MovieAdapter(
+                                    result.data.second.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            0,
+                            PopularConcatAdapter(
+                                MovieAdapter(
+                                    result.data.third.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
                     }
                     //Log.d("LiveData", "Upcoming ${result.data.first} \n \n TopRated: ${result.data.second} \n \n Popular: ${result.data.third}")
-                binding.rvMovies.adapter=concatAdapter
+                    binding.rvMovies.adapter = concatAdapter
                 }
 
                 is Resource.Failure -> {
@@ -87,7 +112,18 @@ class MovieFragment : Fragment(R.layout.fragment_movie), MovieAdapter.OnMovieCli
     }
 
     override fun onMovieClick(movie: Movie) {
-        Log.d("Movie", "onMovieClick: $movie")
+        //Log.d("Movie", "onMovieClick: $movie")
+        val action = MovieFragmentDirections.actionMovieFragmentToMovieDetailFragment(
+            movie.poster_path,
+            movie.backdrop_path,
+            movie.vote_average.toFloat(),
+            movie.vote_count,
+            movie.overview,
+            movie.title,
+            movie.original_language,
+            movie.release_date
+        )
+        findNavController().navigate(action)
     }
 
 
